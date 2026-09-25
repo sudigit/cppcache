f6b0173b1e01e3698f754b7aeee04b298972854f
sudigit <sudikshakadam2020@gmail.com>
2026-09-26 00:10:03 +0530

    one request (length+payload) at a time using readfull and writeall

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,14 +8,7 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-//You can replace read/write with send/recv. 
-// The difference is that send/recv can pass some optional flags 
-// that we don’t need.
 
-// ssize_t read(int fd, void *buf, size_t len);
-// ssize_t recv(int fd, void *buf, size_t len, int flags);         // read
-// ssize_t write(int fd, const void *buf, size_t len);
-// ssize_t send(int fd, const void *buf, size_t len, int flags);   // write
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -26,73 +20,121 @@ static void die(const char *msg) {
     abort();
 }
 
-static void do_something(int connfd) {
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);  //last idx kep for \\'0'
-    if (n < 0) {
-        msg("read() error");
-        return;
-    }
-    fprintf(stderr, "client says: %s\n", rbuf);
+// read abd write can sometimes 
+// return number of bytes read and written lesser than n
+// to not let this happen we have read_full and write_all
+// they make sure exact n  bytes are read/written
 
-    char wbuf[] = "world";
-    write(connfd, wbuf, strlen(wbuf));
+const size_t k_max_msg = 4096;
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
-int main(){
-    // fd is file descriptor which are integers
-    // AF_INET is for IPv4, AF_INET6 for IPv6 or dual-stack sockets.
-    // SOCK_STREAM for TCP, OCK_DGRAM for UDP.
-    // The 3rd argument is 0 and useless for our purposes.
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
-    //3 arg is to specify which protocol 
-    // but we are already doing it by 1st+2nd argu so no need hence 0
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
-    // socket options like
-    // TCP no delay, IP QoS, etc.
-    //  These options are set via the setsockopt() API
+static int32_t one_request(int connfd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        // errno stores the error no 
+        // but EOF is not error so errno will remain 0
+        msg(errno == 0 ? "EOF" : "read() error"); 
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    fprintf(stderr, "client says: %.*s\n", len, &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    // this is needed for most server applications
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
-    //SOL_SOCKET = socket layer
-    //SO_REUSEADDR the option 
-    // val=1 means you enabling option and it requires the pointer to val
 
+    // bind
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
+    int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
 
-    //bind to the wildcard address 0.0.0.0:1234.
-    //  This is just a parameter for listen().
-
-    struct sockaddr_in addr = {};  //define your address
-    addr.sin_family = AF_INET;     //ipv4
-    // now it has something to do with big & little endian
-    addr.sin_port = htons(1234);        // port  (Host to Network Short)
-    addr.sin_addr.s_addr = htonl(0);    // wildcard IP 0.0.0.0   (Host to Network Long)
-    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
-    if (rv) { die("bind()"); }  //rv=0 success, -1=error
-
-     // listen
-     // 2nd aargument is size of queue
-     //SOMAXCONN is 4096 on Linux
+    // listen
     rv = listen(fd, SOMAXCONN);
-    if (rv) { die("listen()"); }
+    if (rv) {
+        die("listen()");
+    }
 
-
-    // loop that accepts and processes each client connection.
     while (true) {
         // accept
         struct sockaddr_in client_addr = {};
         socklen_t addrlen = sizeof(client_addr);
-        // now this is the socket for that particular client 
-        // fd=listening socket, connfd=connected fd
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-
         if (connfd < 0) {
             continue;   // error
         }
 
-        do_something(connfd);
+        while (true) {
+            // here the server only serves one client connection at once
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
         close(connfd);
     }
 
     return 0;
-    
 }
